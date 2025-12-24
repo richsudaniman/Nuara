913c2ebf31f855888b49823bbf410df0d031bd57
Jalal Abdelrahim <jalalr321@gmail.com>
2025-12-24 23:56:02 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -53,7 +53,7 @@ export default function Layout({ children, currentPageName }) {
     
     // Fallback to actual user role/type
     if (user?.role === 'admin') return 'admin';
-    if (user?.user_type === 'trainer') return 'trainer';
+    if (user?.role === 'trainer') return 'trainer';
     return 'client';
   };
 
